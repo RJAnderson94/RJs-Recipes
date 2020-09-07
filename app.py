7f5be3cc6ee4b7847e0f553e5dfeaca73d08ee2d
Ryan Anderson <RJAnderson94@gmail.com>
2020-09-07 12:45:04 +0000

    food, cocktial, recipe and contact pages added and linked together

--- app.py
+++ app.py
@@ -9,6 +9,26 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/cocktail_home')
+def cocktail_home():
+    return render_template('cocktails.html')
+
+
+@app.route('/food_home')
+def food_home():
+    return render_template('food.html')
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html')
+
+
+@app.route('/contact_home')
+def contact_home():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(
         os.environ.get('PORT')), debug=True)
